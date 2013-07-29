4f7281320b91e7331e42b5d63e78c1c08feebb5e
Ronny Bräunlich <r.braeunlich@gmail.com>
2013-07-29 07:55:37 +0200

    Created projects for communication with client

---
+++ modules/prost-client-messaging/src/main/java/de/blogspot/wrongtracks/prost/msg/impl/MessageToClientServiceImpl.java
@@ -0,0 +1,12 @@
+package de.blogspot.wrongtracks.prost.msg.impl;
+
+import de.blogspot.wrongtracks.prost.msg.api.MessageToClientService;
+
+public class MessageToClientServiceImpl implements MessageToClientService {
+
+	public void sendGuiUpdateMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
