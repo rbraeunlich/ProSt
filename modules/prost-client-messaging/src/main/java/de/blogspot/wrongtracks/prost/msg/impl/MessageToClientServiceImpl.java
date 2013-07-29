172715b0fa4a2575a7da914cbb1a6932cdb36375
Ronny Bräunlich <r.braeunlich@gmail.com>
2013-07-29 08:18:20 +0200

    -added properties-file for connection properties -added content.xml

--- modules/prost-client-messaging/src/main/java/de/blogspot/wrongtracks/prost/msg/impl/MessageToClientServiceImpl.java
+++ modules/prost-client-messaging/src/main/java/de/blogspot/wrongtracks/prost/msg/impl/MessageToClientServiceImpl.java
@@ -1,5 +1,7 @@
 package de.blogspot.wrongtracks.prost.msg.impl;
 
+import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,36 +24,50 @@ public class MessageToClientServiceImpl implements MessageToClientService {
 	private Topic topic;
 	private static final Logger logger = Logger
 			.getLogger(MessageToClientServiceImpl.class.getName());
-	private static final String CONNECTION_USER = "prozessstMessaging";
-	private static final String CONNECTION_PWD = "messaging";
+	private Properties props = new Properties();
+	private static final String CONNECTION_USER = "connectionUser";
+	private static final String CONNECTION_PWD = "connectionPwd";
+	private static Connection connection;
 
-	public void sendGuiUpdateMessage() {
+	public void init() {
 		Connection connection = null;
 		try {
-			connection = factory.createConnection(CONNECTION_USER,
-					CONNECTION_PWD);
+			props.load(MessageToClientServiceImpl.class
+					.getResourceAsStream("connection.properties"));
+			connection = factory.createConnection(
+					props.getProperty(CONNECTION_USER),
+					props.getProperty(CONNECTION_PWD));
 			connection.start();
+		} catch (JMSException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void destroy() {
+		try {
+			connection.stop();
+			connection.close();
+		} catch (JMSException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void sendGuiUpdateMessage() {
+		try {
 			Session session = connection.createSession(false,
 					Session.AUTO_ACKNOWLEDGE);
 			MessageProducer messageProducer = session.createProducer(topic);
 			Message message = session.createMessage();
-			logger.log(
-					Level.FINE,
-					"Sending message: " + message.toString() + " with ID: "
-							+ message.getJMSMessageID() + " and topic: "
-							+ topic.getTopicName());
+			logger.log(Level.FINE, "Sending message: " + message.toString()
+					+ " with ID: " + message.getJMSMessageID() + " and topic: "
+					+ topic.getTopicName());
 			messageProducer.send(message);
 			messageProducer.close();
 			session.close();
 		} catch (JMSException e) {
 			throw new RuntimeException(e);
-		} finally {
-			try {
-				connection.stop();
-				connection.close();
-			} catch (JMSException e) {
-				throw new RuntimeException(e);
-			}
 		}
 
 	}
