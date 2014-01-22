809a32230120cf8ef1dfd25dc0a4f1f38fccd386
Ronny Bräunlich <r.braeunlich@gmail.com>
2014-01-22 17:03:53 +0100

    BlueprintELResolver now takes ServiceReferences instead of Service-Proxies

--- modules/activiti-engine-blueprint-wrapper/src/main/java/de/blogspot/wrongtracks/prost/blueprint/ProStBlueprintELResolver.java
+++ modules/activiti-engine-blueprint-wrapper/src/main/java/de/blogspot/wrongtracks/prost/blueprint/ProStBlueprintELResolver.java
@@ -7,11 +7,14 @@ import java.util.logging.Logger;
 import org.activiti.engine.impl.javax.el.ELContext;
 import org.activiti.engine.impl.pvm.delegate.ActivityBehavior;
 import org.activiti.osgi.blueprint.BlueprintELResolver;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
 public class ProStBlueprintELResolver extends BlueprintELResolver {
 	private static final Logger LOGGER = Logger
 			.getLogger(ProStBlueprintELResolver.class.getName());
-	private Map<String, ActivityBehavior> delegateMap = new HashMap<String, ActivityBehavior>();
+	private Map<String, ServiceReference> delegateMap = new HashMap<String, ServiceReference>();
+	private BundleContext context;
 
 	public Object getValue(ELContext context, Object base, Object property) {
 		if (base == null) {
@@ -20,27 +23,33 @@ public class ProStBlueprintELResolver extends BlueprintELResolver {
 			for (String name : delegateMap.keySet()) {
 				if (name.equalsIgnoreCase(key)) {
 					context.setPropertyResolved(true);
-					return delegateMap.get(name);
+					return getContext().getService(delegateMap.get(name));
 				}
 			}
 		}
 		return super.getValue(context, base, property);
 	}
 
-	@SuppressWarnings("rawtypes")
-	public void bindBehaviour(ActivityBehavior behavior, Map props) {
-		String name = (String) props.get("osgi.service.blueprint.compname");
+	public void bindBehaviour(ServiceReference behavior) {
+		String name = behavior.getProperty("osgi.service.blueprint.compname").toString();
 		delegateMap.put(name, behavior);
 		LOGGER.info("Added Activiti behavior to behavior cache " + name);
 	}
 
-	@SuppressWarnings("rawtypes")
-	public void unbindBehaviour(ActivityBehavior behavior, Map props) {
-		String name = (String) props.get("osgi.service.blueprint.compname");
+	public void unbindBehaviour(ServiceReference behavior) {
+		String name = behavior.getProperty("osgi.service.blueprint.compname").toString();
 		if (delegateMap.containsKey(name)) {
 			delegateMap.remove(name);
 		}
 		LOGGER.info("removed Activiti behavior from behavior cache " + name);
+	}
+
+	public BundleContext getContext() {
+		return context;
+	}
+
+	public void setContext(BundleContext context) {
+		this.context = context;
 	}
 
 }
