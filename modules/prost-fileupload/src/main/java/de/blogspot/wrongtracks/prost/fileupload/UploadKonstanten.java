bf6738bb81b0c840a67288cf68f3c2a75b0328d5
Ronny Bräunlich <r.braeunlich@gmail.com>
2014-01-30 16:06:50 +0100

    forgot to change a name

--- modules/prost-fileupload/src/main/java/de/blogspot/wrongtracks/prost/fileupload/UploadKonstanten.java
+++ modules/prost-fileupload/src/main/java/de/blogspot/wrongtracks/prost/fileupload/UploadKonstanten.java
@@ -8,7 +8,7 @@ public interface UploadKonstanten {
 	
 	public static final String REQUEST_PART_DATEI = "datei";
 	
-	public static final String PROST_DATA_DIR_NAME = "Activiti";
+	public static final String PROST_DATA_DIR_NAME = "prost";
 	
 	public static final String PROST_DATA_DIR_PROP = "prost.data.dir";
 
